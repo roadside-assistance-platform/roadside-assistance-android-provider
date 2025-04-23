a0d0c61ab0b839879d35872aa612c8b27cdd0e93
Younes Bouhouche <younes.bouhouche12@gmail.com>
2025-04-23 10:34:30 +0100

    Fixed endpoints

--- app/src/main/java/esi/roadside/assistance/provider/core/data/Endpoints.kt
+++ app/src/main/java/esi/roadside/assistance/provider/core/data/Endpoints.kt
@@ -1,10 +1,10 @@
 package esi.roadside.assistance.provider.core.data
 
 object Endpoints {
-    const val LOGIN = "/client/login"
-    const val SIGNUP = "/client/signup"
-    const val RESET_PASSWORD = "/client/reset-password"
-    const val UPDATE_PROFILE = "/client/update/"
+    const val LOGIN = "/provider/login"
+    const val SIGNUP = "/provider/signup"
+    const val RESET_PASSWORD = "/provider/reset-password"
+    const val UPDATE_PROFILE = "/provider/update/"
     const val HOME = "/home"
     const val SEND_EMAIL = "/email/send-code"
     const val VERIFY_EMAIL = "/email/verify-code"
