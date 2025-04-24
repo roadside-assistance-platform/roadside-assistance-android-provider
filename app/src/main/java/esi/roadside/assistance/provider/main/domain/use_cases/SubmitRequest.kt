97890aefeac1f4e24e3bbe6755a9d05114b399b1
Younes Bouhouche <younes.bouhouche12@gmail.com>
2025-04-24 19:22:01 +0100

    Removed unnecessary files

--- app/src/main/java/esi/roadside/assistance/provider/main/domain/use_cases/SubmitRequest.kt
+++
@@ -1,8 +0,0 @@
-package esi.roadside.assistance.provider.main.domain.use_cases
-
-import esi.roadside.assistance.provider.main.domain.models.AssistanceRequestModel
-import esi.roadside.assistance.provider.main.domain.repository.MainRepo
-
-class SubmitRequest(private val mainRepo: MainRepo) {
-    suspend operator fun invoke(request: AssistanceRequestModel) = mainRepo.submitRequest(request)
-}
--- app/src/main/java/esi/roadside/assistance/provider/main/util/StringToNotification.kt
+++
@@ -1,8 +0,0 @@
-package esi.roadside.assistance.provider.main.util
-
-import esi.roadside.assistance.provider.main.domain.models.NotificationModel
-import java.time.LocalDateTime
-
-fun String.toNotificationModel(createdAt: LocalDateTime): NotificationModel {
-    return NotificationModel("", "", "", false, "", createdAt)
-}
